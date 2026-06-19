897fe809963f96a9e46cbfec17f65dbe9b1c20cc
Dunkyatchkovesh <metalgearsnake427@gmail.com>
2026-06-20 03:30:22 +0530

    controls header file

--- engine/controls.h
+++ engine/controls.h
@@ -1,21 +1,17 @@
-#ifndef CONTROLS_H
-#define CONTROLS_H
-
-#include <stdbool.h>
-
-typedef struct {
-    bool is_paused;
-    bool request_replay;
-    long current_frame;
-} PlaybackState;
-
-void init_controls(PlaybackState *state);
-void toggle_pause(PlaybackState *state);
-void trigger_replay(PlaybackState *state);
-void reset_replay_flag(PlaybackState *state);
-
-#endif
-"""
+const char *controls_h_content = 
+"#ifndef CONTROLS_H\n"
+"#define CONTROLS_H\n\n"
+"#include <stdbool.h>\n\n"
+"typedef struct {\n"
+"    bool is_paused;\n"
+"    bool request_replay;\n"
+"    long current_frame;\n"
+"} PlaybackState;\n\n"
+"void init_controls(PlaybackState *state);\n"
+"void toggle_pause(PlaybackState *state);\n"
+"void trigger_replay(PlaybackState *state);\n"
+"void reset_replay_flag(PlaybackState *state);\n\n"
+"#endif\n";
 
 with open('controls.h', 'w') as f:
     f.write(controls_h_content)
